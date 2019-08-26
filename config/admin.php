c56a95e222c700ec622ae1972bc1ac24f7182359
fengxin <502251245@qq.com>
2019-08-26 12:50:40 +0800

    2019 8 26 修改后台logo

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>强伟纸业</b>',
+    'logo' => '<img src="/vendor/laravel-admin/WechatIMG3.png" alt="强伟纸业" title="强伟纸业" width="120">',
 
     /*
     |--------------------------------------------------------------------------
